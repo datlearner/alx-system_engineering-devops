a6c075368ff05b8d968486d37a9a433bd80bee3b
datlearner <kundaimuchemwa456@gmail.com>
2024-04-22 15:07:57 +0200

    task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""Script that uses JSONPlaceholder API to get information about an employee."""
+""" Script that uses JSONPlaceholder API to get information about employee """
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    base_url = 'https://jsonplaceholder.typicode.com/'
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    user_id = sys.argv[1]
-    user_url = f'{base_url}users/{user_id}'
-    user_res = requests.get(user_url)
-    user_data = user_res.json()
-    print(f"Employee {user_data.get('name')} is done with tasks", end="")
-    todos_url = f'{base_url}todos?userId={user_id}'
-    tasks = todos_res.json()
-    completed_tasks = [task for task in tasks if task.get('completed')]
-    for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_o = res.json()
+    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
+
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            i_task.append(task)
+
+    print("({}/{}):".format(len(l_task), len(tasks)))
+    for task in i_task:
+        print("\t {}".format(task.get("title")))
+            
